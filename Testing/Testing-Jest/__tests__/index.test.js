2cde19886d1ff8e84f35b9253d7badeaeba7a003
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-05-15 17:46:40 -0500

    Testing Unit Test

---
+++ Testing/Testing-Jest/__tests__/index.test.js
@@ -0,0 +1,21 @@
+test('Verificar que 2 + 2 = 4', () => {
+  expect(2 + 2).toBe(4)
+})
+
+test('Verificar que 2 + 2 = 4, pero usando toEqual', () => {
+  expect(2 + 2).toEqual(4)
+})
+
+test('Verificar que 5 es mayor a 4', () => {
+  expect(5).toBeGreaterThan(4)
+})
+
+let holaMundo = 'Hola mundo'
+
+test('Verificar que la palabra Hola este dentro de mi variable holaMundo', () => {
+  expect(holaMundo).toMatch(/Hola/)
+})
+
+test('Verificar que la palabra todos no este dentro de mi variable holaMundo', () => {
+  expect(holaMundo).not.toMatch(/todos/)
+})
